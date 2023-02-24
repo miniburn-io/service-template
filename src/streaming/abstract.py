afc785e5c0f51511c4fd1a796191af9d320da6c0
Vladimir Semenov <sepsseven@gmail.com>
2023-02-24 22:06:24 +0300

    Add listener

--- src/streaming/abstract.py
+++ src/streaming/abstract.py
@@ -5,19 +5,10 @@ from pydantic import BaseModel
 
 class Stream(ABC):
     @abstractmethod
-    def read(self, max_records: int) -> Generator[BaseModel, None, None]:
+    def read(self) -> Generator[BaseModel, None, None]:
         pass
 
     @abstractmethod
     def put(self, message: BaseModel) -> None:
         pass
-
-    @staticmethod
-    def get_full_stream_name(stream_name: str) -> str:
-        ZONE = os.getenv("ZONE")
-        CATALOG_ID = os.getenv("FOLDER_ID")
-        DATABASE_ID = os.getenv("DATABASE_ID")
-
-        return f"/{ZONE}/{CATALOG_ID}/{DATABASE_ID}/{stream_name}"
-
 

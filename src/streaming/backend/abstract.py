34c72f1e8348353d0d95315e85d845d8e564fc3e
Vladimir Semenov <sepsseven@gmail.com>
2023-02-26 00:32:33 +0300

    Add broker

--- src/streaming/backend/abstract.py
+++ src/streaming/backend/abstract.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
-from typing import Generator
+from typing import Generator, Type
 from pydantic import BaseModel
 
 
-class Stream(ABC):
+class StreamBackend(ABC):
+    def __init__(self, topic: str, record_model: Type[BaseModel]):
+        self._stream_name = topic
+        self._record_model = record_model
+
     @abstractmethod
     def read(self) -> Generator[BaseModel, None, None]:
         pass
